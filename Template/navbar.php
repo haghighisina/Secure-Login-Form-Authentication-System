fadd548870b5e6d6f8fe685db2c569450601527d
haghighisina <khabbazhaghighi@gmail.com>
2020-12-18 16:24:33 +0330

    Secure Login

--- Template/navbar.php
+++ Template/navbar.php
@@ -2,24 +2,26 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <?php
-                if(func::checkLoginStat($db))
-                    echo '
+                <?php if(func::checkLoginStat($db)): ?>
                      <a class="nav-link text-white" href="../secure-login/index.php">Home</a>
                      <li class="nav-item">
                         <a class="nav-link text-white" href="../secure-login/admin.php">Admin</a>
                      </li>
-                   ';
-                ?>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="../secure-login/register.php">Register</a>
+                <?php endif;?>
             </li>
         </ul>
-        <ul class="navbar-nav ml-auto">
+       <ul class="navbar-nav ml-auto">
             <li class="nav-item align-items-end">
+                <?php if(func::checkLoginStat($db)): ?>
                 <a class="nav-link text-white" href="../secure-login/logout.php">Logout</a>
+                <?php endif;?>
+                <?php if(!func::checkLoginStat($db)): ?>
+                <a class="nav-link text-white" href="../secure-login/logout.php">Login</a>
+                <?php endif;?>
             </li>
+           <li class="nav-item">
+               <a class="border-white border-left nav-link text-white" href="../secure-login/register.php">Register</a>
+           </li>
         </ul>
     </div>
 </nav>
